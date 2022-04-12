ff6ac0fd3d6bd1cf2023ea3d97f3dc4355178a3c
tcxia <muctianciking@163.com>
2022-04-12 15:02:37 +0800

    gcn

--- gcn/layer.py
+++ gcn/layer.py
@@ -8,7 +8,54 @@ class GraphConv(nn.Module):
         super().__init__()
 
         self.dropout =dropout
-        self.bias = bias
+        # self.bias = bias
         self.activation = activation
         self.is_sparse_inputs = is_sparse_inputs
+        self.featureless = featureless
+        self.num_features = num_features
+
+        self.weight = nn.Parameter(torch.randn(input_dim, output_dim))
+        self.bias = None
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(output_dim))
+
         
+    def forward(self, inputs):
+        x, support = inputs
+        
+        if self.training and self.is_sparse_inputs:
+            x = self.sparse_dropout(x, self.dropout, self.num_features)
+        elif self.training:
+            x = F.dropout(x, self.dropout)
+
+        if not self.featureless:
+            if self.is_sparse_inputs:
+                xw = torch.sparse.mm(x, self.weight)
+            else:
+                xw = torch.mm(x, self.weight)
+        else:
+            xw = self.weight
+
+        out = self.sparse.mm(support, xw)
+        
+        if self.bias is not None:
+            out += self.bias
+        
+        return self.activation(out), support
+
+
+    def sparse_dropout(self, x, rate, noise_shape):
+        random_tensor = 1 - rate
+        random_tensor += torch.rand(noise_shape).to(x.device)
+        dropout_mask = torch.floor(random_tensor).byte()
+        i = x._indices()
+        v = x._values()
+
+        i = i[:, dropout_mask]
+        v = v[dropout_mask]
+
+        out = torch.sparse.FloatTensor(i, v, x.shape).to(x.device)
+
+        out = out * (1. / (1 - rate))
+
+        return out
--- gcn/model.py
+++ gcn/model.py
@@ -1,10 +1,25 @@
+from re import X
 import torch
 import torch.nn as nn
-
+from layer import GraphConv
 
 class GCN(nn.Module):
-    def __init__(self, input_dim, output_dim, num_features) -> None:
+    def __init__(self, input_dim, hidden_dim, output_dim, num_features) -> None:
         super().__init__()
         self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
         self.output_dim = output_dim
-        
+
+        self.layer = nn.Sequential(
+            GraphConv(self.input_dim, self.hidden_dim, num_features, is_sparse_inputs=True),
+            GraphConv(self.hidden_dim, self.output_dim),
+        )
+
+    def forward(self, inputs):
+        x, support = inputs
+        x = self.layer((x, support))
+
+        return x 
+
+
+
