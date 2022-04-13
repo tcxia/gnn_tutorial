987957664988b794857cb06d8d0fe73e241c93ce
tcxia <muctianciking@163.com>
2022-04-13 18:28:15 +0800

    gat

---
+++ gat/layer.py
@@ -0,0 +1,128 @@
+from fileinput import hook_compressed
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GraphAttentionLayer(nn.Module):
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True) -> None:
+        super().__init__()
+        self.dropout = dropout
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+
+        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)
+        self.a = nn.Parameter(torch.empty(size=(2 * out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+    def forward(self, h, adj):
+        Wh = torch.mm(h, self.W)
+        e = self._prepare_attention_input(Wh)
+
+        zero_vec = -9e15 * torch.ones_like(e)
+        attn = torch.where(adj > 0, zero_vec)
+        attn = F.softmax(attn, dim=1)
+        attn = F.dropout(attn, self.dropout, training=self.training)
+        h_prime = torch.matmul(attn, Wh)
+
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+    def _prepare_attention_input(self, Wh):
+        Wh1 = torch.matmul(Wh, self.a[: self.out_features, :])
+        Wh2 = torch.matmul(Wh, self.a[self.out_features :, :])
+        e = Wh1 + Wh2.T
+        return self.leakyrelu(e)
+
+
+class SpecialSpmmFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, indices, values, shape, b):
+        assert indices.required_grad == False
+        a = torch.sparse_coo_tensor(indices, values, shape)
+        ctx.save_for_backward(a, b)
+        ctx.N = shape[0]
+        return torch.matmul(a, b)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        a, b = ctx.saved_tensors
+        grad_values = grad_b = None
+        if ctx.needs_input_grad[1]:
+            grad_a_dense = grad_output.matmul(b.t())
+            edge_idx = a._indices()[0, :] * ctx.N + a._indices()[1, :]
+            grad_values = grad_a_dense.view(-1)[edge_idx]
+        if ctx.needs_input_grad[3]:
+            grad_b = a.t().matmul(grad_output)
+        return None, grad_values, None, grad_b
+
+
+class SpecialSpmm(nn.Module):
+    def forward(self, indices, values, shape, b):
+        return SpecialSpmmFunction.apply(indices, values, shape, b)
+
+
+class SpGraphAttentionLayer(nn.Module):
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True) -> None:
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+
+        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)
+        self.a = nn.Parameter(torch.zeros(size=(1, 2 * out_features)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+        self.dropout = nn.Dropout(dropout)
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+        self.special_spmm = SpecialSpmm()
+
+    def forward(self, input, adj):
+        N = input.size()[0]
+        edge = adj.nonzero().t()
+        h = torch.mm(input, self.W)
+        assert not torch.isnan(h).any()
+
+        edge_h = torch.cat((h[edge[0, :], :], h[edge[1, :], :]), dim=1).t()
+
+        edge_e = torch.exp(-self.leakyrelu(self.a.mm(edge_h).squeeze()))
+        assert not torch.isnan(edge_e).any()
+
+        e_rowsum = self.special_spmm(
+            edge,
+            edge_e,
+            torch.Size([N, N]),
+            torch.ones(size=(N, 1), device=input.device),
+        )
+
+        edge_e = self.dropout(edge_e)
+        
+        h_prime = self.special_spmm(edge, edge_e, torch.Size([N, N]), h)
+        assert not torch.isnan(h_prime).any()
+
+        h_prime = h_prime.div(e_rowsum)
+        assert not torch.isnan(h_prime).any()
+
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+    def __repr__(self):
+        return (
+            self.__class__.__name__
+            + " ("
+            + str(self.in_features)
+            + " -> "
+            + str(self.out_features)
+            + ")"
+        )
